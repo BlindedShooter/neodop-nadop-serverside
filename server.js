c8b6163b8f559132193178d068ee55ceaa4d8230
BlindedShooter <gkandflh@gmail.com>
2018-06-08 15:12:29 +0900

    Fixed /accepthelp's misspell

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ admin.initializeApp({
 var firedb = admin.firestore();
 var userRef = firedb.collection('users');
 
-console.log("======== Server Started ========\n");
-
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -43,19 +41,27 @@ app.post('/requesthelp', (req, res) => {
     var candidates = gcs.search(parseFloat(req.body.lat), parseFloat(req.body.lon), 4.0, 10.0);
     var uid = req.body.uid;
     var req_type = 'request';
+    var res_log = '';
+
+    res.set('Content-Type', 'text/plain');
+
     if (Array.isArray(candidates)) {
         var len = candidates.length;
+        res_log += "[Requesthelp] Help request from lat: " + req.body.lat + ", lon: " + req.body.lon + ", uid: " + uid + "\n[Requesthelp] => Candidates: " + candidates;
         console.log("[Requesthelp] Help request from lat: ", req.body.lat, ", lon: ", req.body.lon, ", uid: ", uid);
         console.log("[Requesthelp] => Candidates: ", candidates);
         // What the hell of the callbacks...... Not understanble nor maintainable.
         if (candidates.length >= minimum_helpers) {
             userRef.doc(uid).get().then(doc => {
                 if (!doc.exists) {
+                    res_log += '\n[Requesthelp] ==== Invalid helpee uid (document not exists on firestore) ====\n';
                     console.log('[Requesthelp] ==== Invalid helpee uid (document not exists on firestore) ====\n');
-                    res.sendStatus(400);
+                    res.status(400);
+                    res.send(res_log);
                 } else {
-                    res.sendStatus(200);
-                    console.log("\n[Requesthelp] =======> Messaging Progress:");
+                    res.status(200);
+                    res.send(res_log);
+                    console.log("[Requesthelp] =======> Messaging Progress:");
                     for (var i = 0; i < len; i++) {
                         userRef.doc(candidates[i]).get()
                             .then(doc => {
@@ -94,24 +100,28 @@ app.post('/requesthelp', (req, res) => {
             })    
         }
         else {
+            res_log += "\n[Requesthelp] Not enough helpers for " + uid + "\n";
             console.log("[Requesthelp] Not enough helpers for ", uid, "\n");
-            res.sendStatus(400);
+            res.status(400);
+            res.send(res_log);
         }
     }
     else {
+        res_log += "[Requesthelp] Invalid help request from " + uid + "\n";
         console.log("[Requesthelp] Invalid help request from ", uid, "\n");
-        res.sendStatus(400);
+        res.status(400);
+        res.send(res_log);
     }
 })
 
 app.post('/accepthelp', (req, res) => {
-    var help_code = res.body.helperuid;
-    var helper_uid_ = res.body.helperuid;
+    var help_code = req.body.helpeeuid;
+    var helper_uid_ = req.body.helperuid;
     var req_type = 'match_success';
     if (ongoing_help.has(help_code)) {
-        res.sendStatus(400);
+        //res.sendStatus(400);
     }
-    else {
+    //else {
         userRef.doc(req.body.helpeeuid).get()
             .then(doc=> {
                 if (!doc.exists) {
@@ -135,11 +145,11 @@ app.post('/accepthelp', (req, res) => {
             })
         ongoing_help.set(help_code, 1);
         res.sendStatus(200);
-    }
+    //}
 })
 
 app.post('/finishhelp', (req, res) => {
-    var help_code = res.body.helperuid;
+    var help_code = req.body.helpeeuid;
     if (ongoing_help.has(help_code)) {
         ongoing_help.delete(help_code);
         res.sendStatus(200);
@@ -156,4 +166,13 @@ app.get('/show_all_helpers', (req, res) => {
     res.status(200);
     res.send("==== Available Helpers ====\n" + gcs.get_users());
 })
+
+app.get('/timestamp', (req, res) => {
+    var t = new Date().getTime();
+    res.set('Content-Type', 'text/plain');
+    res.status(200);
+    res.send('' + t);
+})
+
+console.log("======== Server Started ========\n");
 app.listen(8000);
